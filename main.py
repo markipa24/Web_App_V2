08e384c0ed1f2c179731b8f9aca4dbfe743d6ce8
markipa24 <96586800+markipa24@users.noreply.github.com>
2023-06-02 15:22:47 +0000

    Added jobitem.html

--- main.py
+++ main.py
@@ -2,9 +2,34 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+
+JOBS = [
+    {
+      'id':1,
+      'title': 'Structural Engineer',
+      'location': 'Philippines',
+      'salary': 'P30,000'
+    },
+    {
+      'id':2,
+      'title': 'Computational Engineer',
+      'location': 'HongKong',
+      'salary': 'P50,000'
+    },
+    {
+      'id':3,
+      'title': 'Project Manager',
+      'location': 'Australia',
+      'salary': 'P150,000'
+    }
+]
+
 @app.route("/")
+
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
