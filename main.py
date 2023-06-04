496a97579a27f1da65b9228ebe839d2340f8bfe1
markipa24 <96586800+markipa24@users.noreply.github.com>
2023-06-04 02:20:26 +0000

    Added dynamic data and api route

--- main.py
+++ main.py
@@ -1,43 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-
-
-JOBS = [
-    {
-      'id':1,
-      'title': 'Structural Engineer',
-      'location': 'Philippines',
-      'salary': 'P30,000'
-    },
-    {
-      'id':2,
-      'title': 'Computational Engineer',
-      'location': 'HongKong',
-      'salary': 'P50,000'
-    },
-    {
-      'id':3,
-      'title': 'Project Manager',
-      'location': 'Australia',
-      'salary': 'P150,000'
-    },
-    {
-      'id':4,
-      'title': 'Senior Structural Engineer',
-      'location':'UAE',
-      'salary': 'P450,000'
-    }
-  ]
+JOBS = [{
+  'id': 1,
+  'title': 'Structural Engineer',
+  'location': 'Philippines',
+  'salary': 'P25,000'
+}, {
+  'id': 2,
+  'title': 'Computational Engineer',
+  'location': 'HongKong',
+  'salary': 'P50,000'
+}, {
+  'id': 3,
+  'title': 'Project Manager',
+  'location': 'Australia',
+  'salary': 'P150,000'
+}]
 
 
 @app.route("/")
-
 def hello_world():
-    return render_template('home.html', jobs=JOBS)
+  return render_template('home.html', jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',debug=True)
-  
+  app.run(host='0.0.0.0', debug=True)
